64bbec5d4cc0decb255858bbe5483879b694d34f
Maciej Michalec <maciej.michalec@gmail.com>
2017-02-01 10:26:15 +0100

    Added widgets in Recognize Voice tab

--- RecognizeVoiceTab.cpp
+++ RecognizeVoiceTab.cpp
@@ -2,29 +2,66 @@
 #include <QLabel>
 #include <QLineEdit>
 #include <QBoxLayout>
+#include <QPushButton>
 
 RecognizeVoiceTab::RecognizeVoiceTab(QWidget *parent)
     : QWidget(parent)
 {
     resize(300, 300);
-    QLabel *fileNameLabel = new QLabel(tr("File Name:"));
-    QLineEdit *fileNameEdit = new QLineEdit("fileInfo.fileName()");
 
-    QLabel *pathLabel = new QLabel(tr("Path:"));
-    QLabel *pathValueLabel = new QLabel("fileInfo.absoluteFilePath()");
-    pathValueLabel->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+    QVBoxLayout* rootLayout = new QVBoxLayout;
+    rootLayout->setContentsMargins(0, 0, 0, 0);
 
-    QLabel *lastModLabel = new QLabel(tr("Last Modified:"));
-    QLabel *lastModValueLabel = new QLabel("fileInfo.lastModified().toString()");
-    lastModValueLabel->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+    QLabel* selectFileLabel = new QLabel("Select audio file");
+    QFont f( "Arial", 15, QFont::Bold);
+    selectFileLabel->setFont(f);
+    selectFileLabel->setAlignment(Qt::AlignTop | Qt::AlignCenter);
+    selectFileLabel->setMaximumHeight(20);
+    rootLayout->addWidget(selectFileLabel);
 
-    QVBoxLayout *mainLayout = new QVBoxLayout;
-    mainLayout->addWidget(fileNameLabel);
-    mainLayout->addWidget(fileNameEdit);
-    mainLayout->addWidget(pathLabel);
-    mainLayout->addWidget(pathValueLabel);
-    mainLayout->addWidget(lastModLabel);
-    mainLayout->addWidget(lastModValueLabel);
-    mainLayout->addStretch(1);
-    setLayout(mainLayout);
+    QHBoxLayout* filesLayout = new QHBoxLayout;
+    QLineEdit* selectedFileEdit = new QLineEdit("");
+    QPushButton* selectButton = new QPushButton("...");
+    QPushButton* compareFilesButton = new QPushButton("Compare");
+    selectedFileEdit->setDisabled(true);
+    compareFilesButton->setDisabled(true);
+    filesLayout->addWidget(selectedFileEdit);
+    filesLayout->addWidget(selectButton);
+    rootLayout->addLayout(filesLayout);
+    rootLayout->addWidget(compareFilesButton);
+
+    QFrame* line = new QFrame(this);
+    line->setObjectName(QString::fromUtf8("line"));
+    line->setGeometry(QRect(320, 150, 118, 3));
+    line->setFrameShape(QFrame::HLine);
+    line->setFrameShadow(QFrame::Sunken);
+    rootLayout->addWidget(line);
+
+    QLabel* recordVoiceLabel = new QLabel("Record your voice");
+    recordVoiceLabel->setFont(f);
+    recordVoiceLabel->setAlignment(Qt::AlignTop | Qt::AlignCenter);
+    recordVoiceLabel->setMaximumHeight(20);
+    rootLayout->addWidget(recordVoiceLabel);
+
+    QHBoxLayout* recordingsButtonLayout = new QHBoxLayout;
+    QPushButton* recordButton = new QPushButton("Record");
+    QPushButton* stopButton = new QPushButton("Stop and compare");
+    stopButton->setDisabled(true);
+    recordingsButtonLayout->addWidget(recordButton);
+    recordingsButtonLayout->addWidget(stopButton);
+    rootLayout->addLayout(recordingsButtonLayout);
+
+
+    QFrame* line2 = new QFrame(this);
+    line2->setObjectName(QString::fromUtf8("line"));
+    line2->setGeometry(QRect(320, 150, 118, 3));
+    line2->setFrameShape(QFrame::HLine);
+    line2->setFrameShadow(QFrame::Sunken);
+    rootLayout->addWidget(line2);
+
+    QLabel* resultLabel = new QLabel("");
+    resultLabel->setMaximumHeight(100);
+    rootLayout->addWidget(resultLabel);
+
+    setLayout(rootLayout);
 }
