3d272c3f97faf639ae2e46e7dea69f743fe96500
Maciej Michalec <maciej.michalec@gmail.com>
2017-02-02 23:54:54 +0100

    Implemented adding own recordings (ugly code, not refactorized yet)

--- AddAudioFileTab.h
+++ AddAudioFileTab.h
@@ -5,6 +5,7 @@
 #include <QPushButton>
 #include <QLabel>
 #include <QLineEdit>
+#include <QAudioRecorder>
 
 class AddAudioFileTab : public QWidget
 {
@@ -21,13 +22,22 @@ public slots:
     void selectFileClicked();
     void checkIfAddButonCanBeEnabled();
 
+    void selectAudioOutputClicked();
+    void recordClicked();
+    void stopClicked();
+    void updateProgress(qint64 progress);
+
 private:
     QLineEdit* selectedFileEdit;
     QLineEdit* titleEdit;
     QLineEdit* authorEdit;
     QPushButton* addButton;
     QLabel* resultLabel;
-
+    QLabel* recordingInfoLabel;
+    QAudioRecorder* audioRecorder;
+    QLineEdit* recordingFilepathEdit;
+    QPushButton* recordButton;
+    QPushButton* stopButton;
 };
 
 #endif // ADDAUDIOFILETAB_H
