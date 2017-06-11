8d26ebf8248db6e874a5b1961bddab71286889e2
Maciej Michalec <maciej.michalec@gmail.com>
2017-06-11 16:27:50 +0200

    Getting current user in basket service

--- E-Business/Project/frontend/src/app/basket/basket.service.ts
+++ E-Business/Project/frontend/src/app/basket/basket.service.ts
@@ -1,21 +1,30 @@
 import { Injectable } from '@angular/core';
 import {Http, Headers, RequestOptions, URLSearchParams} from "@angular/http";
 import {Basket} from "./basket";
+import { LoginService } from '../login/login.service';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class BasketService {
 
-  constructor(private http: Http) { }
+  profile: any;
+
+  constructor(private loginService: LoginService, private http: Http) { }
 
   getBasket() {
     console.log("Get basket");
+
     const headers: Headers = new Headers();
     headers.append('Accept', 'application/json');
     headers.append('Content-Type', 'application/json');
 
+
+    this.profile = this.loginService.userProfile;
+    var userId: string = this.profile.sub; //TODO: check if sub exists
+    console.log("UserID=" + userId);
+
     let params: URLSearchParams = new URLSearchParams();
-    params.set('id', "fb:1234"); //TODO: get current user id (oauth2)
+    params.set('id', userId); //TODO: get current user id (oauth2)
 
     const options = new RequestOptions({headers: headers, search: params});
 
